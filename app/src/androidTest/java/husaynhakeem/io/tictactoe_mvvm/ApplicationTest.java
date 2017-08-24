9bb5a8c0113cdbec324049c0b5d9bee5c7d9ac8c
Husayn Hakeem <husaynhakeem@gmail.com>
2017-08-24 17:17:10 +0100

    Gradle dependencies config

---
+++ app/src/androidTest/java/husaynhakeem/io/tictactoe_mvvm/ApplicationTest.java
@@ -0,0 +1,11 @@
+package husaynhakeem.io.tictactoe_mvvm;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
