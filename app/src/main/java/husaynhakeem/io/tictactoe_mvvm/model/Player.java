d7b4f5f37248c95e216185e2ce5ed3ac9e50ed64
andreasstravelas <andreasds@csd.auth.gr>
2021-09-26 21:34:21 +0300

    fix issues

--- app/src/main/java/husaynhakeem/io/tictactoe_mvvm/model/Player.java
+++ app/src/main/java/husaynhakeem/io/tictactoe_mvvm/model/Player.java
@@ -3,11 +3,19 @@ package husaynhakeem.io.tictactoe_mvvm.model;
 
 public class Player {
 
-    public String name;
-    public String value;
+    private String name;
+    private String value;
 
     public Player(String name, String value) {
         this.name = name;
         this.value = value;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getValue(){
+        return this.value;
     }
 }
